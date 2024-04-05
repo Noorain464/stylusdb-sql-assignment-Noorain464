c94353154d391ea617672dedac564a95a048617a
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-04-05 22:14:01 +0530

    solved 5

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ const parseQuery = require('./queryParser');
 const readCSV = require('./csvReader');
 
 async function executeSELECTQuery(query) {
-    const { fields, table } = parseQuery(query);
+    const { fields, table, whereClause } = parseQuery(query);
     const data = await readCSV(`${table}.csv`);
     
+    const filteredData = whereClause
+        ? data.filter(row => {
+            const [field, value] = whereClause.split('=').map(s => s.trim());
+            return row[field] === value;
+        })
+        : data;
     // Filter the fields based on the query
-    return data.map(row => {
+    return filteredData.map(row => {
         const filteredRow = {};
         fields.forEach(field => {
             filteredRow[field] = row[field];
